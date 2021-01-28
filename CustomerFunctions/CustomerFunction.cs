e7256a6f9a6ddf9134baadd8ef51b116a91d6f85
MJendza <mjendza@newforma.com>
2021-01-28 23:15:59 +0100

    added validation

--- CustomerFunctions/CustomerFunction.cs
+++ CustomerFunctions/CustomerFunction.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using CustomerFunctions.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -22,9 +23,17 @@ namespace CustomerFunctions
             log.LogInformation($"environment: {env}");
             string name = req.Query["name"];
 
-            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name ??= data?.name;
+            log.LogInformation("POST C# HTTP request.");
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            log.LogInformation($"POST C# HTTP request body {requestBody}.");
+            var request = JsonConvert.DeserializeObject<CreateCustomer>(requestBody);
+            
+            var validator = new CustomerValidator();
+            var validationResult = await validator.ValidateAsync(request);
+            if (!validationResult.IsValid)
+            {
+                throw new ArgumentException(validationResult.Errors.ToString());
+            }
 
             var responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
---
+++ CustomerFunctions/Models/CreateCustomer.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace CustomerFunctions.Models
+{
+    public class CreateCustomer
+    {
+        public string Name { get; set; }
+    }
+    public class CustomerValidator: AbstractValidator<CreateCustomer> {
+        public CustomerValidator() {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name can't be empty.");
+        }
+    }
+}
