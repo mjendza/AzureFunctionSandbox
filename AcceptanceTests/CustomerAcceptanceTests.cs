b526c4190c96ae92478cd3f5e9df9fdb174d0d7c
MJendza <mjendza@newforma.com>
2021-02-07 23:43:10 +0100

    prettier

--- AcceptanceTests/CustomerAcceptanceTests.cs
+++ AcceptanceTests/CustomerAcceptanceTests.cs
@@ -1,5 +1,3 @@
-using Microsoft.Extensions.Primitives;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,6 +5,8 @@ using AzureFunctions.AcceptanceTest.Runner;
 using CustomerFunctions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Primitives;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AcceptanceTests
 {
@@ -20,7 +20,7 @@ namespace AcceptanceTests
             var body = "{\"name\":\"yamada\"}";
             var req = HttpRequestSetup(query, body, "get");
             var result = await AzureFunctionInvoker.Invoke((request) => CustomerFunction.Run(request, logger), req);
-            var resultObject = (OkObjectResult) result;
+            var resultObject = (OkObjectResult)result;
             Assert.AreEqual("Hello, yamada. This HTTP triggered function executed successfully.", resultObject.Value);
         }
     }
--- AzureFunctions.AcceptanceTest.Runner/FunctionTest.cs
+++ AzureFunctions.AcceptanceTest.Runner/FunctionTest.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.IO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Internal;
